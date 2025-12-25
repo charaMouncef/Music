0c072b3aafd369bb0f6277fba2ab558d480f1a7f
mouncef <chmouncef@gmail.com>
2025-12-25 21:17:03 +0100

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="h-screen flex items-center justify-center">
+      <Text className="text-xl text-red-500">Edit aniggerpp/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
